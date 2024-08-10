1b57bb62d3522ff3afe25f03eebd9078937b8284
aravind <aravindsanalkumar@gmail.com>
2024-08-10 14:25:09 +0530

    selectors added

---
+++ script.js
@@ -0,0 +1,25 @@
+// DOM Manipulation 
+
+
+//getElementById()
+
+let title = document.getElementById('main-heading');
+
+
+//getElementByClassName()
+
+let listItem = document.getElementsByClassName('list_items');
+
+
+//getElementByTagName()
+let listItemByTag = document.getElementsByTagName('li');
+
+
+
+//querySelector()
+let container = document.querySelector('div');
+
+
+//querySelectorAll()
+
+let liAll = document.querySelectorAll('li');
