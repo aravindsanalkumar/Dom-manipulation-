d30557c41e42f432b20156285feca1c9f1a69a00
aravind <aravindsanalkumar@gmail.com>
2024-08-10 15:38:18 +0530

    dom traversal and nodes completed

--- script.js
+++ script.js
@@ -56,8 +56,43 @@ ul.append(extraElement);
 extraElement.textContent = 'I N T E R S T E L L A R ';
 extraElement.classList.add('list_item')
 console.log(extraElement.getAttribute('class'));
+console.log(extraElement.classList.contains('list_item'))
 extraElement.classList.remove('list_item');
 newElement.remove();
 extraElement.remove();
 li.remove();
+
+//Traversing the DOM
+
+
+//Parent Node traversal
+
+let ulT = document.querySelector('ul');
+console.log(ulT.parentElement.parentElement);
+console.log(ulT.parentNode);
+
+const html = document.documentElement;
+console.log(html);
+console.log(html.parentElement);
+console.log(html.parentNode);
+
+//Child Node traversal
+console.log(ulT.childNodes);
+console.log(ulT.firstChild);
+console.log(ulT.lastChild);
+ulT.childNodes[3].style.backgroundColor = 'blue'
+
+console.log(ulT.children);
+console.log(ulT.firstElementChild);
+console.log(ulT.lastElementChild);
+
+//Sibling Node Traversal
+
+const ulS = document.querySelector('ul');
+console.log(ulS.previousSibling);
+console.log(ulS.nextSibling);
+
+
+
+
 
