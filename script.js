ef4a8a5f13a97e737bbe3bd46bd6777cc7150c8a
aravind <aravindsanalkumar@gmail.com>
2024-08-10 14:56:31 +0530

    modifying classes and attributes

--- script.js
+++ script.js
@@ -3,23 +3,58 @@
 
 //getElementById()
 
-let title = document.getElementById('main-heading');
+let titlexxx = document.getElementById('main-heading');
 
 
 //getElementByClassName()
 
-let listItem = document.getElementsByClassName('list_items');
+let listItemxxx = document.getElementsByClassName('list_items');
 
 
 //getElementByTagName()
-let listItemByTag = document.getElementsByTagName('li');
+let listItemByTagxxx = document.getElementsByTagName('li');
 
 
 
 //querySelector()
-let container = document.querySelector('div');
+let containerxxx = document.querySelector('div');
 
 
 //querySelectorAll()
 
-let liAll = document.querySelectorAll('li');
+let liAllxxx = document.querySelectorAll('li');
+
+
+//Dom manipulation 
+
+const title = document.querySelector('#main_heading');
+title.style.color = 'red';
+const listItems =  document.querySelectorAll('.list_items');
+for(let i = 0;i < listItems.length;i++){
+    listItems[i].style.backgroundColor = 'lightblue';
+}
+
+//Creating Elements
+const ul = document.querySelector('ul');
+const li = document.createElement('li');
+ul.append(li);
+li.textContent = 'Spiderman';
+
+const firstListItem = document.querySelector('.list_items');
+console.log(firstListItem.innerHTML);
+console.log(firstListItem.innerText);
+console.log(firstListItem.textContent);
+
+//Modifying attribute and classes
+const newElement = document.createElement('li');
+ul.append(newElement);
+newElement.textContent = "A V A T A R";
+newElement.setAttribute('class','list_item');
+newElement.removeAttribute('class','list_item');
+const extraElement = document.createElement('li');
+ul.append(extraElement);
+extraElement.textContent = 'I N T E R S T E L L A R ';
+extraElement.classList.add('list_item')
+console.log(extraElement.getAttribute('class'));
+extraElement.classList.remove('list_item')
+
